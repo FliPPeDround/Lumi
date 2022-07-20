7c272f80891c3aaed12fd2a895c61b944df9958d
flippedround <734243792@qq.com>
2022-07-20 23:57:00 +0800

    chore : init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import LumiVideo from '/Lumi.mp4'
+import LumiPoster from '/Lumi.jpeg'
+const width = window.screen.width
+const height = window.screen.height
+
+</script>
+
+<template>
+  <video :src="LumiVideo" :poster="LumiPoster" autoplay loop :width="width" :height="height"></video>
+</template>
+
+<style>
+body {
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
+</style>
